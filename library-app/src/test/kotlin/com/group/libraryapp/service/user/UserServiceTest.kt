bc4cbebe3cc18a2efcbed26e02c8ef10d34a7c99
cjy9249 <cjy920409@gmail.com>
2022-12-02 18:28:53 +0900

    feat : deleteTest 유저를 삭제해주는 테스트

--- library-app/src/test/kotlin/com/group/libraryapp/service/user/UserServiceTest.kt
+++ library-app/src/test/kotlin/com/group/libraryapp/service/user/UserServiceTest.kt
@@ -3,8 +3,10 @@ package com.group.libraryapp.service.user
 import com.group.libraryapp.domain.user.User
 import com.group.libraryapp.domain.user.UserRepository
 import com.group.libraryapp.dto.user.request.UserCreateRequest
+import com.group.libraryapp.dto.user.request.UserUpdateRequest
 import org.assertj.core.api.AssertionsForInterfaceTypes.assertThat
 import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -52,7 +54,34 @@ class UserServiceTest @Autowired constructor(
         assertThat(result).extracting("name").containsExactlyInAnyOrder("A","B") //["A","B"]
         assertThat(result).extracting("age").containsExactlyInAnyOrder(20,30) //["A","B"]
     }
+    @Test
+    @DisplayName("유저 업데이트가 정상동작 테스트")
+    fun updateUserNameTest(){
+        //given
+        val savedUser = userRepository.save(User("A",20))
+        val request = UserUpdateRequest(savedUser.id,"B")
 
 
+        //when
+        userService.updateUserName(request)
+
+        //then
+        val result = userRepository.findAll()[0]
+        assertThat(result.name).isEqualTo("B")
+
+
+    }
+
+    @Test
+    @DisplayName("유저 삭제가 정상작동하는지 테스트")
+    fun deleteUserTest(){
+        userRepository.save(User("A",23))
+
+        //when
+        userService.deleteUser("A")
+
+        //then
+        assertThat(userRepository.findAll()).isEmpty()
+    }
 
 }
